778091f2872db5c5896d1a61519496fc97087cfc
JKBit <jonaskaz2009@outlook.de>
2025-03-22 21:34:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.post("/changequeue", (req, res) => {
-  res.send(req.body);
+  console.log(req.body);
 });
 
 app.get("/", (req, res) => {
