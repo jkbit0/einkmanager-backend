17764f266fb440ca84995634a202f1ad11448537
JKBit <jonaskaz2009@outlook.de>
2025-03-25 15:30:21 +0100

    Implemented Basic Functionality
    Added Slides.json File for slides
    Added Changing and Reading of Slides.json file

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-app.post("/changequeue", (req, res) => {
-  console.log(req.body);
+app.post("/changeslidequeue", async (req, res) => {
+  console.log("Changed queue to: " + JSON.stringify(req.body));
+  let data = JSON.stringify(req.body);
+  fs.writeFileSync("slides.json", data);
+  res.send("Change Slidequeue succesfully!");
 });
 
 app.get("/", (req, res) => {
-  res.send("Queue");
+  let rawdata = fs.readFileSync("slides.json");
+  let slides = JSON.parse(rawdata);
+  res.send(slides);
 });
 
 app.listen(port, () => {
